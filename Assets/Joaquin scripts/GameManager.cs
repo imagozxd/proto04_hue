7e8ef4fdfcdb33c6f04135b09ca22eb5bae09c36
CondoriJoaquin <130480676+Yoinzar@users.noreply.github.com>
2024-10-18 11:20:18 -0500

    Agregando Player

--- Assets/Joaquin scripts/GameManager.cs
+++ Assets/Joaquin scripts/GameManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private PlayerHUE player;
+    public GameObject gameOverPanel;
+    public GameObject gameWinPanel;
     [SerializeField] private HandlerEventoSO handlerfloat;
     [SerializeField] private int vida;
+
+    public GameManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+        //gameOverPanel.SetActive(false);
+        //gameWinPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
     public void WinGame()
     {
-        
+        Time.timeScale = 0f;
+        //gameWinPanel.SetActive(true);
+
     }
     public void LoseGame()
     {
-
+        Time.timeScale = 0f;
+        //gameOverPanel.SetActive(true);
     }
 
 }
+
+
